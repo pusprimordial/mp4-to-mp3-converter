878ddbeb8a9e0a328c424fd28024fdb03128af87
googleinferno <pedrovieirau8@gmail.com>
2023-07-24 16:40:27 -0300

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from pytube import YouTube
 import os
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
@@ -18,12 +20,16 @@ def download():
 
         default_filename = audio_stream.default_filename
         mp3_filename = os.path.splitext(default_filename)[0] + '.mp3'
-        os.rename(default_filename, mp3_filename)
+        os.rename(default_filename, 'static/' + mp3_filename)
 
-        return render_template('download.html', mp3_filename=mp3_filename)
+        return redirect('/download/' + mp3_filename)
 
     except Exception as e:
         return render_template('error.html', error=str(e))
+
+@app.route('/download/<mp3_filename>')
+def download_mp3(mp3_filename):
+    return render_template('download.html', mp3_filename=mp3_filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
